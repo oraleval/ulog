be349ef0d1b37c6e63fe24c90bfc438bc0fb3dbd
huangruiqi <huangruiqi@unisound.com>
2020-03-24 14:04:07 +0800

    修改日志分析增加本地缓存和定时器

---
+++ plugin/filter/log_filter.go
@@ -0,0 +1,170 @@
+package filter
+
+import (
+	"github.com/ouqiang/timewheel"
+	"github.com/tidwall/gjson"
+	"github.com/xujiajun/nutsdb"
+	"io"
+	"log"
+	"strings"
+	"sync"
+	"time"
+)
+
+var (
+	logBlack = sync.Map{}
+	DBBucket = "logBucket"
+)
+
+type LogData struct {
+	RequestID string `json:"requestID"`
+	GlobalID  string `json:"globalID"`
+	Level     string `json:"level"`
+}
+
+type LogCacheData struct {
+	IsError bool
+}
+
+type LogFilter struct {
+	W  io.Writer
+	DB *nutsdb.DB
+	TW *timewheel.TimeWheel
+}
+
+func NewLogFilter(w io.Writer, file string) *LogFilter {
+	l := &LogFilter{W: w}
+
+	//_ = os.RemoveAll(file)
+	opt := nutsdb.DefaultOptions
+	opt.Dir = file
+	db, err := nutsdb.Open(opt)
+	if err != nil {
+		log.Printf("new nutsdb err:%v\n", err)
+		return l
+	}
+
+	l.TW = LogCacheReload(l)
+	l.DB = db
+	l.TW.Start()
+
+	return l
+}
+
+func (f *LogFilter) Write(p []byte) (n int, err error) {
+	level := gjson.Get(string(p), "level").String()
+	globalID := gjson.Get(string(p), "globalID").String()
+	if globalID != "" {
+		tmp, isGlobalID := logBlack.Load(globalID)
+		if strings.LastIndex(string(p), "end") > 0 {
+			err := f.SendOut(globalID, p, tmp)
+			if err != nil {
+				log.Printf("NewLogFilter SendOut err (%v)\n", err)
+				return len(p), err
+			}
+			return len(p), nil
+		}
+		if !isGlobalID {
+			logBlack.Store(globalID, false)
+			f.TW.AddTimer(10*time.Second, globalID, globalID)
+		}
+		if level == "error" {
+			logBlack.Store(globalID, true)
+		}
+		err := f.AddLogCache(globalID, p)
+		if err != nil {
+			log.Printf("NewLogFilter AddLogCache err (%v)\n", err)
+			return f.W.Write(p)
+		}
+		return len(p), nil
+	}
+	if level == "error" {
+		return f.W.Write(p)
+	}
+	return len(p), nil
+}
+
+func (f *LogFilter) SendOut(globalID string, p []byte, tmp interface{}) (err error) {
+	logBlack.Delete(globalID)
+	time.Sleep(time.Second)
+	newList, err := f.GetLogCache(globalID)
+	if err != nil {
+		log.Printf("GetLogCache globalID (%s) err (%v)\n", globalID, err)
+	}
+	err = f.DeleteLogCache(globalID)
+	if err != nil {
+		log.Printf("DeleteLogCache err (%v)\n", err)
+	}
+	if len(newList) > 0 && tmp.(bool) {
+		for _, n := range newList {
+			_, err := f.W.Write(n)
+			if err != nil {
+				log.Printf("GetLogCache list Write err (%v)\n", err)
+			}
+		}
+		f.W.Write(p)
+	}
+	f.TW.RemoveTimer(globalID)
+	return nil
+}
+
+func (f *LogFilter) AddLogCache(globalID string, msg []byte) error {
+	err := f.DB.Update(func(tx *nutsdb.Tx) error {
+		key := []byte(globalID)
+		return tx.SAdd(DBBucket, key, msg)
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (f *LogFilter) GetLogCache(globalID string) ([][]byte, error) {
+	list := make([][]byte, 0)
+	err := f.DB.View(func(tx *nutsdb.Tx) error {
+		key := []byte(globalID)
+		if items, err := tx.SMembers(DBBucket, key); err != nil {
+			return err
+		} else {
+			for _, n := range items {
+				list = append(list, n)
+			}
+		}
+		return nil
+	})
+	return list, err
+}
+
+func (f *LogFilter) DeleteLogCache(globalID string) error {
+	err := f.DB.Update(func(tx *nutsdb.Tx) error {
+		key := []byte(globalID)
+		if items, err := tx.SMembers(DBBucket, key); err != nil {
+			return err
+		} else {
+			if len(items) > 0 {
+				for _, n := range items {
+					err := tx.SRem(DBBucket, key, n)
+					if err != nil {
+						return err
+					}
+				}
+			}
+		}
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func LogCacheReload(l *LogFilter) *timewheel.TimeWheel {
+	t := timewheel.New(2*time.Second, 3600, func(data interface{}) {
+		logBlack.Delete(data)
+		err := l.DeleteLogCache(data.(string))
+		if err != nil {
+			log.Printf("DeleteLogCache globalID (%v) err (%v)\n", data, err)
+		}
+	})
+	return t
+}
--- plugin/filter/log_filter_test.go
+++ plugin/filter/log_filter_test.go
@@ -1,7 +1,9 @@
-package ulog
+package filter
 
 import (
 	"fmt"
+	"github.com/oraleval/ulog"
+	"github.com/oraleval/ulog/plugin/nsqclient"
 	"io"
 	"os"
 	"testing"
@@ -11,16 +13,14 @@ import (
 // Test_LogFilter
 func Test_LogFilter(t *testing.T) {
 	var w []io.Writer
-	nsqClient := NewNsqClient("ai-service", "eval", "192.168.6.100:4161")
-	if nsqClient == nil {
+	client := nsqclient.NewNsqClient("ai-service", "eval", "192.168.6.100:4161")
+	if client == nil {
 		fmt.Printf("new nsql client fail\n")
 		return
 	}
-	w = append(w, NewLogFilter(nsqClient), os.Stdout)
-	l := New(append(w)...).SetLevel("info")
-	l1 := l.With().Str("hostName", "bj01").Str("serverName", "eval01").Logger()
-	l1.Info().Msgf("test")
-	l1.Error().Msgf("test")
+	t1 := NewLogFilter(client, "/tmp/fileDB")
+	w = append(w, t1)
+	l := ulog.New(append(w)...).SetLevel("info")
 	l2 := l.With().Str("hostName", "bj01").Str("serverName", "eval01").Str("requestID", "requestIDTest").Str("globalID", "test22").Logger()
 	//l2.Error().Msg("print1 error")
 	l2.Info().Msg("print1 info")
@@ -30,5 +30,16 @@ func Test_LogFilter(t *testing.T) {
 	time.Sleep(1 * time.Second)
 	l2.Info().Msg("print3 info")
 	l2.Error().Msg("print2 error")
-	time.Sleep(5 * time.Second)
+
+	//list, _ := t1.GetLogCache("test22")
+	//fmt.Println(list)
+	time.Sleep(1000 * time.Second)
+}
+
+func Test_File(t *testing.T) {
+	f := NewLogFilter(os.Stdout, "/tmp/fileDB")
+	list, _ := f.GetLogCache("test22")
+	for _, i := range list {
+		fmt.Println(string(i))
+	}
 }
